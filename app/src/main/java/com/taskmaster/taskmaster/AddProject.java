236a89b3011a87559113f8171ff7f2d68ea92660
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-01-28 15:34:59 -0800

    make UI changes

---
+++ app/src/main/java/com/taskmaster/taskmaster/AddProject.java
@@ -0,0 +1,27 @@
+package com.taskmaster.taskmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+    }
+
+    public void onCreateProjectButtonClick(View v){
+
+        // grab the info from the form
+        EditText title = findViewById(R.id.editText);
+        EditText description = findViewById(R.id.editText2);
+
+        // add to the database
+
+        // redirect to the main page
+        finish();
+    }
+}
