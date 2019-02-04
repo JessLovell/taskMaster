a59a5db77d43b72849cf638ad263ce71c9131a06
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-02-03 16:08:29 -0800

    view all tasks for user

---
+++ app/src/main/java/com/taskmaster/taskmaster/AllTask.java
@@ -0,0 +1,13 @@
+package com.taskmaster.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_task);
+    }
+}
